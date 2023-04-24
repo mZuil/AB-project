a2384d78452024a6964788b6c0a6df81ff9e8f30
mZuil <maria20zg00@gmail.com>
2023-04-24 18:36:42 +0200

    Front-end page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AListItemsComponent } from './a-list-items/a-list-items.component';
 import { BListItemsComponent } from './b-list-items/b-list-items.component';
+import { FormsModule } from '@angular/forms';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
     AppComponent,
     AListItemsComponent,
-    BListItemsComponent
+    BListItemsComponent,
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    FontAwesomeModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
+}
